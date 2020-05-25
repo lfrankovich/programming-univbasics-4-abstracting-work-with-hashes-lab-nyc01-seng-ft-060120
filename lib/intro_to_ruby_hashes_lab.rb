9a41601be67e54d50b92b0d20007a58e1a4f253d
Lauren Frankovich <frankovister@gmail.com>
2020-05-25 02:41:06 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -23,21 +23,3 @@ end
 	else
 	puts "Key not found!"
 end
-
-      it "if key is not present, returns a hash with the provided key assigned to the value of 1" do
-        expect(update_counting_hash({},'hello')).to eq({'hello' => 1})
-        expect(update_counting_hash({total: 5},:age)).to eq({total: 5, age: 1})
-      end
-  
-      it "if key is present, returns a hash where the key's value is incremented by 1" do
-        expect(update_counting_hash({count: 5},:count)).to eq({count: 6})
-        expect(update_counting_hash({'number' => 1, 'amount' => 50},'number')).to eq({'number' => 2, 'amount' => 50})
-      end
-    end
-
-end
-
-describe "reading data from a hash" do
-  
-end
-end
